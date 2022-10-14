ebc350fdcff1aafe3185cde60729b0997d999d7c
Almir <crafteasy@yandex.ru>
2022-10-14 13:12:31 +0300

    пытаюсь решить задачу первого  семинара по java

---
+++ Java_seminar/seminar_1/task_1_models.java
@@ -0,0 +1,15 @@
+package Java_seminar.seminar_1;
+
+public class task_1_models {
+    public static int K1( int a,  int c) {
+        int mult = 0;
+        mult = mult + (a * c);
+        return mult;
+    }
+
+    public static int K2( int a,  int d) {
+        int sum = 0;
+        sum = sum + (a + d);
+        return sum;
+    }
+}
---
+++ Lesson_1/1.java
@@ -0,0 +1,10 @@
+package Lesson_1;
+
+public class 1 {
+    public static void main(String[] args) {
+        int a = 123;
+        a = --a - a--;
+        System.out.println(a);
+    }
+  
+}
