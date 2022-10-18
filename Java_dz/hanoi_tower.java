4cdd5f6e074897218a8aa6fc6849cf70d18f6574
Almir <crafteasy@yandex.ru>
2022-10-18 17:00:05 +0300

    нашел в интеренете один вариант хонойской башни, пока не сильно понятно

---
+++ Java_dz/hanoi_tower.java
@@ -0,0 +1,17 @@
+package Java_dz;
+
+public class hanoi_tower {
+    public static void main(String[] args) {
+        int n = 3;  
+        tower_hanoi(n, 'A', 'B', 'C');
+    }
+    public static void tower_hanoi(int N, char oneShaft, char twoShaft, char threeShaft) {
+        if (N == 1) {
+            System.out.println("Диск 1 из " + oneShaft + " на " + threeShaft);
+        } else {
+            tower_hanoi(N - 1, oneShaft, threeShaft, twoShaft);
+            System.out.println("Диск " + N + " из " + oneShaft + " на " + threeShaft);
+            tower_hanoi(N - 1, twoShaft, oneShaft, threeShaft);
+        }
+    }
+}
